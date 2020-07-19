6b1ad3f00aa57f66dcdc583746e63f7d52265760
suntzu93 <thanhlv93@gmail.com>
2020-07-19 15:17:21 +0700

    Update EcdsaBoned.ts
    Update transaction when bonded change status

--- src/EcdsaBoned.ts
+++ src/EcdsaBoned.ts
@@ -8,31 +8,34 @@ import {
 } from "../generated/schema";
 
 import {
-  getOrCreateTotalBondedECDSAKeep
+  getOrCreateTransaction
 } from "./utils/helpers";
 
 export function handleKeepClosed(event: KeepClosed): void {
-    let ownerAddress = event.transaction.to.toHex();
-    let bondedEcdsaKeep = BondedECDSAKeep.load(ownerAddress);
-    if(bondedEcdsaKeep != null){
-      bondedEcdsaKeep.state = "CLOSED";
-      bondedEcdsaKeep.save()
-
-      let totalBonded = getOrCreateTotalBondedECDSAKeep();
-      totalBonded.totalAmount.minus(bondedEcdsaKeep.bondAmount);
-      totalBonded.save();
-    }
+  let ownerAddress = event.transaction.to.toHex();
+  let bondedEcdsaKeep = BondedECDSAKeep.load(ownerAddress);
+  if (bondedEcdsaKeep != null) {
+    let transaction = getOrCreateTransaction(event.transaction.hash.toHex());
+    transaction.timestamp = event.block.timestamp;
+    transaction.blockNumber = event.block.number;
+    transaction.save();
+  
+    bondedEcdsaKeep.state = "CLOSED";
+    bondedEcdsaKeep.transaction = transaction.id;
+    bondedEcdsaKeep.save()
+  }
 }
 
 export function handleKeepTerminated(event: KeepTerminated): void {
   let ownerAddress = event.transaction.to.toHex();
   let bondedEcdsaKeep = BondedECDSAKeep.load(ownerAddress);
-  if(bondedEcdsaKeep != null){
+  if (bondedEcdsaKeep != null) {
+    let transaction = getOrCreateTransaction(event.transaction.hash.toHex());
+    transaction.timestamp = event.block.timestamp;
+    transaction.blockNumber = event.block.number;
+    transaction.save();
+  
     bondedEcdsaKeep.state = "TERMINATED";
     bondedEcdsaKeep.save()
-
-    let totalBonded = getOrCreateTotalBondedECDSAKeep();
-    totalBonded.totalAmount.minus(bondedEcdsaKeep.bondAmount);
-    totalBonded.save();
   }
 }
